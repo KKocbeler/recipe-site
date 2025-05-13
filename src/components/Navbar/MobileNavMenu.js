c4d094f3795e2fd8d01b4945d3e21ad32e259993
Kemal Koçbeler <kkocbeler@gmail.com>
2025-05-13 13:26:34 +0300

    update MobileNavMenu component, convert to SCSS and apply minor style changes

--- src/components/Navbar/MobileNavMenu.js
+++ src/components/Navbar/MobileNavMenu.js
@@ -1,38 +1,64 @@
-import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './MobileNavMenu.css';
+import './MobileNavMenu.scss';
+import { useEffect, useState } from 'react';
 
 const MobileNavMenu = () => {
     const location = useLocation()
+    const [showMobileNav, setShowMobileNav] = useState(false)
+    const mobileNavLinks = [
+        {
+            path: '/',
+            iconClass: 'fa-solid fa-house',
+            label: 'Home',
+        },
+        {
+            path: '/recipes',
+            search: '?q=meat',
+            iconClass: 'fa-solid fa-bowl-food',
+            label: 'Recipes',
+        },
+        {
+            path: '/article',
+            iconClass: 'fa-solid fa-book-open',
+            label: 'Article',
+        },
+        {
+            path: '/contact',
+            iconClass: 'fa-solid fa-phone',
+            label: 'Contact',
+        },
+    ];
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 0) {
+                setShowMobileNav(true)
+            } else {
+                setShowMobileNav(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => (
+            window.removeEventListener("scroll", handleScroll)
+        )
+
+    }, [])
 
     return (
-        <div className="mobile-nav-menu">
+        <div className={`mobile-nav-menu ${showMobileNav ? "show" : ""}`}>
             <ul className="mobile-list">
-                <li>
-                    <Link to={'/'} className={`mobile-link ${location.pathname === '/' ? 'active' : ''}`}>
-                        <i class="fa-solid fa-house"></i>
-                        <span>Home</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/recipes/?q=meat'} className={`mobile-link ${location.pathname === '/recipes/' ? 'active' : ''}`}>
-                        <i class="fa-solid fa-bowl-food"></i>
-                        <span>Recipes</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/article'} className={`mobile-link ${location.pathname === '/article' ? 'active' : ''}`}>
-                        <i class="fa-solid fa-book-open"></i>
-                        <span>Article</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/contact'} className={`mobile-link ${location.pathname === '/contact' ? 'active' : ''}`}>
-                        <i class="fa-solid fa-phone"></i>
-                        <span>Contact</span>
-                    </Link>
-                </li>
+                {
+                    mobileNavLinks.map((navLink, index) => (
+                        <li key={index}>
+                            <Link to={'/'} className={`mobile-link ${location.pathname === navLink.path ? 'active' : ''}`}>
+                                <i className={navLink.iconClass}></i>
+                                <span>{navLink.label}</span>
+                            </Link>
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
