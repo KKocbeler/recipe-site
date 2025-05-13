e1b32c0d98ce1be0fdbaa9d2dbd72b1dbba0d4f8
Kemal Koçbeler <kkocbeler@gmail.com>
2025-05-13 13:29:29 +0300

    redesign Error component and add SCSS styling

--- src/components/Pieces/Error.js
+++ src/components/Pieces/Error.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import './Error.css';
+import './Error.scss';
 import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
         <div id='error' className='container'>
-            
-            <h2>Error!</h2>
+            <div className="oops">Oops</div>
             <p>We can't seem to find the recipes you're looking for.</p>
             <div>
-                <i className="fa-solid fa-face-frown-open"></i>
+                The page you are looking for does not exist
             </div>
-            <button>
-                <Link to={'/'}>
-                    <i className="fa-solid fa-house"></i>
-                </Link>
-            </button>
-            
+            <Link className='to-home' to={'/'}>Home Page</Link>
         </div>
     )
 }
