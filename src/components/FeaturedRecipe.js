dd99244bf11ef45ddeecc31ae57ec0b21aa169ef
Kemal Koçbeler <kkocbeler@gmail.com>
2025-05-13 13:22:44 +0300

    refactor: use map to render repeated elements in FeaturedRecipe.js

--- src/components/FeaturedRecipe.js
+++ src/components/FeaturedRecipe.js
@@ -1,43 +1,49 @@
-import React from 'react';
-import './FeaturedRecipe.css'
+import './FeaturedRecipe.scss';
 import { Link } from 'react-router-dom';
 
-const FeaturedRecipe = () => {
-  return (
-     <div className='featured-bg'>
-        <div className='side-line-title'>
-            <div className="h-line"></div>
-            <span className='hi leads'> Recipes & Cooking Ideas</span>
-            <div className="h-line"></div>
-        </div>
-        <div id='featured'>
-            <div className="featured-images">
-                <Link to={'/recipes/?q=soup'}>
-                    <img src="Images/FeaturedRecipe/Soop.jpg" className='rounded' alt="" />
-                </Link>
-                <button type='button' className='featured-btn'>Soups</button>
-            </div>
-            <div className="featured-images">
-                <Link to={'/recipes/?q=pasta'}>
-                    <img src="Images/FeaturedRecipe/Pasta.webp" className='rounded' alt="" />
-                    <button className='featured-btn'>Pasta</button>
-                </Link>
-            </div>
-            <div className="featured-images">
-                <Link to={'/recipes/?q=cookie'}>
-                    <img src="Images/FeaturedRecipe/Coocies.jpg" className='rounded' alt="" />
-                    <button className='featured-btn'>Cookies</button>
-                </Link>
-            </div>
-            <div className="featured-images">
-                <Link to={'/recipes/?q=cake'}>
-                    <img src="Images/FeaturedRecipe/dessert.jpg" className='rounded' alt="" />
-                    <button className='featured-btn'>Cakes</button>
-                </Link>
-            </div>
-        </div>
-    </div>
-  )
-}
+const featuredItems = [
+  {
+    title: 'Soups',
+    image: 'Images/FeaturedRecipe/Soop.jpg',
+    query: 'soup'
+  },
+  {
+    title: 'Pasta',
+    image: 'Images/FeaturedRecipe/Pasta.webp',
+    query: 'pasta'
+  },
+  {
+    title: 'Cookies',
+    image: 'Images/FeaturedRecipe/Coocies.jpg',
+    query: 'cookie'
+  },
+  {
+    title: 'Cakes',
+    image: 'Images/FeaturedRecipe/dessert.jpg',
+    query: 'cake'
+  }
+];
 
-export default FeaturedRecipe
+const FeaturedRecipe = () => {
+    return (
+        <div className='featured-bg'>
+            <div className='side-line-title'>
+                <div className="h-line"></div>
+                <h2 className='title leads'> Recipes & Cooking Ideas</h2>
+                <div className="h-line"></div>
+            </div>
+            <div id='featured'>
+                {featuredItems.map((item, index) => (
+                    <div className="featured-images" key={index}>
+                        <Link to={`/recipes/?q=${item.query}`}>
+                        <img src={item.image} className='rounded' alt={item.title} />
+                        <button className='featured-btn'>{item.title}</button>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+    };
+
+export default FeaturedRecipe;
